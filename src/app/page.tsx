805d3033d254982be3d2e23451d86bac9b7a5ecc
MD ZAID123 <zaid20525@iiitd.ac.in>
2025-08-11 00:41:20 +0530

    03:trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+// "use client"
+import React, { Suspense, use } from 'react'
+import { useTRPC } from '@/trpc/client'
+import { dehydrate, HydrationBoundary, useQuery } from '@tanstack/react-query';
 
-const page = () => {
+
+
+import {caller, getQueryClient,trpc} from '@/trpc/server';
+import { Client } from './client';
+const page = async() => {
+  // const trpc=useTRPC();
+  // const {data}=useQuery(trpc.createAI.queryOptions({ text: 'Antonio' }));
+  // trpc.createAI.queryOptions({ text: 'world' });
+  // instead of doing localhost:3000/api/create-ai"
+
+  // const data=await caller.createAI({ text: 'Antonio' });
+
+
+
+  // console.log("SERVER COMPONENT"); 
+
+  const queryClient=getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"Antonio PREFETCH"}));
+  //prefetch query does not return anything
+  
   return (
-    <div>page</div>
+   <HydrationBoundary state={dehydrate(queryClient)}>
+    <Suspense fallback={<p>Loading...</p>}>
+
+    
+    <Client/>
+    </Suspense>
+   </HydrationBoundary>
   )
 }
 
