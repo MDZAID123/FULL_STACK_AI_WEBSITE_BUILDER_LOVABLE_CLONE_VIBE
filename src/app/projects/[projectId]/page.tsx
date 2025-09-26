171834e741a086a588fa71630442699259c44753
MD ZAID123 <zaid20525@iiitd.ac.in>
2025-09-26 19:32:31 +0530

    10:messages ui

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -4,11 +4,45 @@ interface Props{
     }>
 };
 
-import React from 'react'
+import { ProjectView } from '@/modules/projects/ui/views/project-view';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import React, { Suspense } from 'react'
+
+
+
+
 
 const page =async ({params}:Props) => {
+
+    const {projectId}=await params;
+
+    const queryClient=await getQueryClient();
+
+    void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({
+        projectId,
+    }))
+
+    // also prefetching project data before renderijg 
+    void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({
+        id:projectId,
+
+
+    }))
   return (
-    <div>page</div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+
+        <Suspense fallback={<p>Loading ...</p>}>
+
+        
+
+        <ProjectView projectId={projectId}/>
+
+        </Suspense>
+
+    </HydrationBoundary>
+        
+    
   )
 }
 
---
+++ src/modules/projects/ui/views/project-view.tsx
@@ -0,0 +1,149 @@
+"use client"
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { Tabs } from "@radix-ui/react-tabs";
+interface Props{
+    projectId:string;
+}
+export const ProjectView=({projectId}:Props)=>{
+    const trpc=useTRPC();
+
+    const {data:project}=useSuspenseQuery(trpc.projects.getOne.queryOptions({
+        id:projectId,
+    }))
+
+    const {data:messages}=useSuspenseQuery(trpc.messages.getMany.queryOptions({
+        projectId:projectId,
+    }))
+
+    return (
+
+        // <div>
+        //     {JSON.stringify(project)}
+        //     {JSON.stringify(messages,null,2)}
+        // </div>
+        <div className="h-screen">
+            <ResizablePanelGroup direction="horizontal">
+
+                <ResizablePanel
+                defaultSize={35}
+                minSize={20}
+                className="flex flex-col min-h-0">
+                    <ErrorBoundary fallback={<p>Project header Error</p>}>
+                        <Suspense fallback={<p>Loading project ...</p>}>
+                            <ProjectHeader projectId={projectId}/>
+                        </Suspense>
+
+
+                    </ErrorBoundary>
+
+
+                    <ErrorBoundary fallback={<p>Message container Error</p>}>
+                        <Suspense fallback={<p>Loading messages...</p>}>
+                        <MessagesContainer
+
+                        projectId={projectId}
+                        activeFragment={activeFragment}
+                        setActiveFragment={setActiveFragment}
+
+
+/>
+
+                        </Suspense>
+
+                    </ErrorBoundary>
+                    
+
+
+                </ResizablePanel>
+
+
+                <ResizableHandle className="hover:bg-primary transition-colors"/>
+                {/* The Above is a drag bar btw 2 resizable panels */}
+
+                <ResizablePanel
+                default={65}
+                minSize={50}
+                >
+
+                    <Tabs
+                    className="h-full gap-y-0"
+                    defaultValue="preview"
+                    value={tabState}
+                    onValueChange={(value)=>setTabState(value as "preview"|"code")}
+
+                    >
+
+                        <div className="w-full flex items-center p-2 border-b gap-x-2">
+                            <TabList className="h-8 p-0 border rounded-md">
+                                <TabTrigger value="preview"  className="rounded-md">
+
+                                    <EyeIcon/> <span>Demo</span>
+
+
+
+
+                                </TabTrigger>
+
+
+                                <TabsTrigger value="code" className="rounded-md">
+                                    <CodeIcon/><span>Code</span>
+
+                                </TabsTrigger>
+
+                            </TabList>
+
+                            <div className="ml-auto flex items-center gap-x-2">
+
+                                {!hasProAccess && (
+                                    <Button asChild size="sm" variant="tertiary">
+                                        <Link href="pricing">
+
+                                        <CrownIcon/>Upgrade
+                                        </Link>
+
+                                    </Button>
+                                )}
+
+                                <UserControl/>
+
+                            </div>
+
+                        </div>
+
+                        <TabsContent  value="preview">
+                            {!!activeFragment && <FragmentWeb data={activeFragment}/>}
+
+
+                        </TabsContent>
+                        <TabsContent value="code" className="min-h-0">
+                            {!!activeFragment?.files && (
+                                <FileExplorer
+                                files={activeFragment.files as {[path:string]:string}}
+                                />
+                            )}
+                        </TabsContent>
+
+                    </Tabs>
+
+
+                </ResizablePanel>
+
+            </ResizablePanelGroup>
+
+        </div>
+    )
+
+
+}
